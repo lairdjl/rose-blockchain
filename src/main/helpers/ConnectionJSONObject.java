4b9cd02b0e54020c7efd7e0f8da1ed4a8540b8e9
JakeLaird <laird.jacob@gmail.com>
2018-04-18 23:36:12 -0400

    testing...

--- src/main/helpers/ConnectionJSONObject.java
+++ src/main/helpers/ConnectionJSONObject.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 
 public class ConnectionJSONObject implements JSONObjectInterface {
 
-        private static ArrayList<String> connections = new ArrayList<>();
+        public static ArrayList<String> connections = new ArrayList<>();
 //        private static ConnectionJSONObject instance = new ConnectionJSONObject();
         public ConnectionJSONObject(ArrayList<String> connections) {
                 this.connections = connections;
